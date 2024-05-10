8000f20667556504daa0ccd18bee6a8954e189a0
Oivind Thorsen Kulsrud <oivind.thorsen.kulsrud@nav.no>
2024-05-10 14:30:05 +0200

    wip

--- src/main/kotlin/no/nav/helse/flex/api/FlexIdenterCacheApi.kt
+++ src/main/kotlin/no/nav/helse/flex/api/FlexIdenterCacheApi.kt
@@ -3,23 +3,29 @@ package no.nav.helse.flex.api
 import no.nav.helse.flex.clientidvalidation.ClientIdValidation
 import no.nav.helse.flex.repository.IdenterRepository
 import no.nav.security.token.support.core.api.ProtectedWithClaims
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
+
+// import finnAlleDistinctTags
+// import no.nav.helse.flex.repository.findAlleDistinctTags
+
 
 @RestController
 class FlexIdenterCacheApi(
+    @Autowired
     private val identerRepository: IdenterRepository,
     private val clientIdValidation: ClientIdValidation,
 ) {
     @GetMapping("/api/v1/intern/identer")
     @ResponseBody
     @ProtectedWithClaims(issuer = "azureator")
-    fun hentAlleTags(): String {
+    fun hentIdenter(): Set<String> {
         clientIdValidation.validateClientId(
             ClientIdValidation.NamespaceAndApp(
                 namespace = "flex",
                 app = "flex-identer-cache",
             ),
         )
-        return "Hello, world"
+        return identerRepository.finnAlleDistinctTags()
     }
 }
