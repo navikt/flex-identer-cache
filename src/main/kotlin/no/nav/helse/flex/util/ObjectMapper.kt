02556d4ac90249f58cf7fca4bd037df2b6a9c675
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-07-12 15:20:01 +0200

    mer opprydding

--- src/main/kotlin/no/nav/helse/flex/util/ObjectMapper.kt
+++ src/main/kotlin/no/nav/helse/flex/util/ObjectMapper.kt
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 
-val OBJECT_MAPPER =
+val OBJECT_MAPPER: ObjectMapper =
     ObjectMapper()
         .registerModule(JavaTimeModule())
         .registerKotlinModule()
