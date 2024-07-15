b070730b59535098bd129dde9e76207ac0c0dcff
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-07-15 16:54:25 +0200

    prøver logge i dev

--- src/test/kotlin/no/nav/helse/flex/AktorTest.kt
+++ src/test/kotlin/no/nav/helse/flex/AktorTest.kt
@@ -11,6 +11,7 @@ import no.nav.helse.flex.util.tilOsloZone
 import org.amshove.kluent.`should be equal to`
 import org.amshove.kluent.`should be in range`
 import org.amshove.kluent.`should not be`
+import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource
@@ -22,6 +23,7 @@ class AktorTest : FellesTestOppsett() {
     @Autowired
     private lateinit var namedParameterJdbcTemplate: NamedParameterJdbcTemplate
 
+    @Disabled
     @Test
     fun `les identer fra topic`() {
         val ident =
