51b5c39488133212a4e0645a53e7ef5a2f958932
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-08-01 14:01:21 +0200

    tilbake til setup som funker

--- src/main/kotlin/no/nav/helse/flex/kafka/AivenKafkaConfig.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/AivenKafkaConfig.kt
@@ -3,7 +3,9 @@ package no.nav.helse.flex.kafka
 import io.confluent.kafka.serializers.KafkaAvroDeserializer
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig
 import org.apache.avro.generic.GenericRecord
+import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.common.config.SslConfigs
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -15,24 +17,49 @@ import org.springframework.kafka.listener.ContainerProperties
 
 @Configuration
 class AivenKafkaConfig(
-    @Value("\${spring.kafka.bootstrap-servers}") private val kafkaBrokers: String,
-    @Value("\${spring.kafka.properties.schema.registry.url}") private val schemaRegistryUrl: String,
+    @Value("\${KAFKA_BROKERS}") private val kafkaBrokers: String,
+    @Value("\${KAFKA_SCHEMA_REGISTRY}") private val schemaRegistryUrl: String,
+    @Value("\${KAFKA_SCHEMA_REGISTRY_USER}") private val schemaRegistryUser: String,
+    @Value("\${KAFKA_SCHEMA_REGISTRY_PASSWORD}") private val schemaRegistryPassword: String,
+    @Value("\${KAFKA_SECURITY_PROTOCOL:SSL}") private val kafkaSecurityProtocol: String,
+    @Value("\${KAFKA_TRUSTSTORE_PATH}") private val kafkaTruststorePath: String,
+    @Value("\${KAFKA_CREDSTORE_PASSWORD}") private val kafkaCredstorePassword: String,
+    @Value("\${KAFKA_KEYSTORE_PATH}") private val kafkaKeystorePath: String,
 ) {
+    fun commonConfig() =
+        mapOf(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to kafkaBrokers,
+        ) + securityConfig()
+
+    private fun securityConfig() =
+        mapOf(
+            CommonClientConfigs.SECURITY_PROTOCOL_CONFIG to kafkaSecurityProtocol,
+            // Disables server host name verification.
+            SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG to "",
+            SslConfigs.SSL_TRUSTSTORE_TYPE_CONFIG to "JKS",
+            SslConfigs.SSL_KEYSTORE_TYPE_CONFIG to "PKCS12",
+            SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG to kafkaTruststorePath,
+            SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG to kafkaCredstorePassword,
+            SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG to kafkaKeystorePath,
+            SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG to kafkaCredstorePassword,
+            SslConfigs.SSL_KEY_PASSWORD_CONFIG to kafkaCredstorePassword,
+        )
+
     @Bean
     fun consumerFactory(): ConsumerFactory<String, GenericRecord> {
         val props =
             HashMap<String, Any>(
                 mapOf(
-                    ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to kafkaBrokers,
                     ConsumerConfig.GROUP_ID_CONFIG to "flex-identer-cache",
                     ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
                     ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to KafkaAvroDeserializer::class.java,
                     KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG to false,
                     KafkaAvroDeserializerConfig.SCHEMA_REGISTRY_URL_CONFIG to schemaRegistryUrl,
+                    KafkaAvroDeserializerConfig.USER_INFO_CONFIG to "$schemaRegistryUser:$schemaRegistryPassword",
                 ),
             )
 
-        return DefaultKafkaConsumerFactory(props)
+        return DefaultKafkaConsumerFactory(props + commonConfig())
     }
 
     @Bean
