de867aa5a09b8cd8cce2cc93a7410fbca82f4a6d
Nils Jørgen Mittet <nils.jorgen.mittet@nav.no>
2024-08-08 10:46:26 +0200

    Log schemaRegistryUrl

--- src/main/kotlin/no/nav/helse/flex/kafka/AktorConsumer.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/AktorConsumer.kt
@@ -1,10 +1,12 @@
 package no.nav.helse.flex.kafka
+
 import no.nav.helse.flex.logger
 import no.nav.helse.flex.repository.Aktor
 import no.nav.helse.flex.repository.AktorService
 import no.nav.helse.flex.util.Metrikk
 import org.apache.avro.generic.GenericRecord
 import org.apache.kafka.clients.consumer.ConsumerRecord
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
@@ -14,6 +16,7 @@ import java.util.concurrent.ArrayBlockingQueue
 class AktorConsumer(
     private val metrikk: Metrikk,
     private val aktorService: AktorService,
+    @Value("\${KAFKA_SCHEMA_REGISTRY}") val schemaRegistryUrl: String,
 ) {
     val log = logger()
     val buffer = ArrayBlockingQueue<Aktor>(1000)
@@ -31,9 +34,12 @@ class AktorConsumer(
         acknowledgment: Acknowledgment,
     ) {
         try {
+            log.info("schemaRegistryUrl: $schemaRegistryUrl")
             val key = consumerRecord.key()
             val value = consumerRecord.value().toString()
             log.info("key: $key, value: $value")
+        } catch (e: Exception) {
+            log.error(e.message)
         } finally {
             acknowledgment.acknowledge()
         }
