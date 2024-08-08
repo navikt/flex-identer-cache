1f34c9ee6da0e9e534f8a906f7c3ce1ecef1bc3d
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-08-08 11:29:02 +0200

    tester å lagre aktor

--- src/main/kotlin/no/nav/helse/flex/kafka/AktorConsumer.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/AktorConsumer.kt
@@ -4,12 +4,16 @@ import no.nav.helse.flex.logger
 import no.nav.helse.flex.repository.Aktor
 import no.nav.helse.flex.repository.AktorService
 import no.nav.helse.flex.util.Metrikk
+import no.nav.helse.flex.util.osloZone
+import no.nav.helse.flex.util.serialisertTilString
+import no.nav.helse.flex.util.toAktor
 import org.apache.avro.generic.GenericRecord
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.kafka.support.Acknowledgment
 import org.springframework.stereotype.Component
+import java.time.OffsetDateTime
 import java.util.concurrent.ArrayBlockingQueue
 
 @Component
@@ -38,6 +42,15 @@ class AktorConsumer(
             val key = consumerRecord.key()
             val value = consumerRecord.value().toString()
             log.info("key: $key, value: $value")
+
+            val aktorId = Aktor.sanitizeKey(consumerRecord.key())
+            val aktor = consumerRecord.value().toAktor(aktorId)
+            aktor.also {
+                it.identifikatorer.forEach { identifikator -> identifikator.oppdatert = OffsetDateTime.now(osloZone) }
+            }
+            log.info("Forsøker å lagre aktør: ${aktor.serialisertTilString()}")
+            buffer.offer(aktor)
+            aktorService.lagreAktor(aktor)
         } catch (e: Exception) {
             log.error(e.message)
         } finally {
--- src/main/kotlin/no/nav/helse/flex/repository/AktorRepository.kt
+++ src/main/kotlin/no/nav/helse/flex/repository/AktorRepository.kt
@@ -58,7 +58,22 @@ data class Aktor(
     var aktorId: String = "",
     @MappedCollection(idColumn = "aktor_id", keyColumn = "aktor_id")
     var identifikatorer: List<Identifikator> = mutableListOf(),
-)
+) {
+    companion object {
+        fun sanitizeKey(key: String): String {
+            // strips off all non-ASCII characters
+            var text = key
+            text = text.replace("[^\\x00-\\x7F]".toRegex(), "")
+
+            // erases all the ASCII control characters
+            text = text.replace("\\p{Cntrl}&&[^\r\n\t]".toRegex(), "")
+
+            // removes non-printable characters from Unicode
+            text = text.replace("\\p{C}".toRegex(), "")
+            return text.trim().filter { it.isDigit() }
+        }
+    }
+}
 
 data class Identifikator(
     @Id
