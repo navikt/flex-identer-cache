9154068d63c11b9717747b2c79c60161cb9237e9
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-10-30 14:22:55 +0100

    trigger ny lesing

--- src/main/kotlin/no/nav/helse/flex/kafka/AktorConsumer.kt
+++ src/main/kotlin/no/nav/helse/flex/kafka/AktorConsumer.kt
@@ -24,7 +24,7 @@ class AktorConsumer(
     @KafkaListener(
         topics = [AKTOR_TOPIC],
         // TODO endre ved prodsetting
-        id = "flex-identer-cache-v9",
+        id = "flex-identer-cache-v10",
         idIsGroup = true,
         containerFactory = "kafkaAvroListenerContainerFactory",
         properties = ["auto.offset.reset = earliest"],
