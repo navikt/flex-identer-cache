055f1b44ea07c2e79fd3e3797752064f2dbd781f
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-10-30 15:00:01 +0100

    bedre feilhåndtering for verifisering

--- src/main/kotlin/no/nav/helse/flex/repository/AktorService.kt
+++ src/main/kotlin/no/nav/helse/flex/repository/AktorService.kt
@@ -1,5 +1,6 @@
 package no.nav.helse.flex.repository
 
+import no.nav.helse.flex.logger
 import no.nav.helse.flex.util.OBJECT_MAPPER
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Service
@@ -7,15 +8,23 @@ import org.springframework.transaction.annotation.Transactional
 
 @Service
 class AktorService(private val redisTemplate: RedisTemplate<String, String>) {
-    fun verifiserAktor(aktor: Aktor) {
-        requireNotNull(aktor.aktorId) { "Aktor ID kan ikke være null" }
-        val identifikatorer = requireNotNull(aktor.identifikatorer) { "Identifikatorer kan ikke være null" }
+    val log = logger()
 
-        identifikatorer.forEach { identifikator ->
-            requireNotNull(identifikator.idnummer) { "ID-nummer kan ikke være null" }
-            requireNotNull(identifikator.oppdatert) { "Oppdatert kan ikke være null" }
-            requireNotNull(identifikator.type?.name) { "Type kan ikke være null" }
-            requireNotNull(identifikator.gjeldende) { "Gjeldende kan ikke være null" }
+    fun verifiserAktor(aktor: Aktor): Boolean {
+        try {
+            requireNotNull(aktor.aktorId) { "Aktor ID kan ikke være null" }
+            val identifikatorer = requireNotNull(aktor.identifikatorer) { "Identifikatorer kan ikke være null" }
+
+            identifikatorer.forEach { identifikator ->
+                requireNotNull(identifikator.idnummer) { "ID-nummer kan ikke være null" }
+                requireNotNull(identifikator.oppdatert) { "Oppdatert kan ikke være null" }
+                requireNotNull(identifikator.type?.name) { "Type kan ikke være null" }
+                requireNotNull(identifikator.gjeldende) { "Gjeldende kan ikke være null" }
+            }
+            return true
+        } catch (e: Exception) {
+            log.error("Feil på aktør med aktørId ${aktor.aktorId}", e.message)
+            return false
         }
     }
 
@@ -30,10 +39,11 @@ class AktorService(private val redisTemplate: RedisTemplate<String, String>) {
     fun lagreFlereAktorer(aktorList: List<Aktor>) {
         redisTemplate.executePipelined { connection ->
             aktorList.forEach { aktor ->
-                verifiserAktor(aktor)
-                val aktorString = OBJECT_MAPPER.writeValueAsString(aktor)
-                connection.stringCommands()
-                    .set(aktor.aktorId!!.toByteArray(), aktorString.toByteArray())
+                if (verifiserAktor(aktor)) {
+                    val aktorString = OBJECT_MAPPER.writeValueAsString(aktor)
+                    connection.stringCommands()
+                        .set(aktor.aktorId!!.toByteArray(), aktorString.toByteArray())
+                }
             }
             null // Return null since executePipelined expects a return type
         }
