86d738b9e703e1eecd7e95d30740a2edc970138a
Ole Bastian Løchen <ole.bastian.kolstad.lochen@nav.no>
2024-11-06 10:51:15 +0100

    gjør om til post

--- src/main/kotlin/no/nav/helse/flex/api/FlexIdenterCacheApi.kt
+++ src/main/kotlin/no/nav/helse/flex/api/FlexIdenterCacheApi.kt
@@ -1,12 +1,12 @@
 package no.nav.helse.flex.api
 
 import no.nav.helse.flex.clientidvalidation.ClientIdValidation
+import no.nav.helse.flex.config.OIDCIssuer.AZUREATOR
 import no.nav.helse.flex.model.Aktor
 import no.nav.helse.flex.model.AktorService
 import no.nav.security.token.support.core.api.ProtectedWithClaims
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
-import no.nav.helse.flex.config.OIDCIssuer.AZUREATOR
 
 @RestController
 class FlexIdenterCacheApi(
@@ -14,10 +14,12 @@ class FlexIdenterCacheApi(
     private val aktorService: AktorService,
     private val clientIdValidation: ClientIdValidation,
 ) {
-    @GetMapping("/api/v1/identer/{aktorId}")
+    @PostMapping("/api/v1/identer/aktor")
     @ResponseBody
     @ProtectedWithClaims(issuer = AZUREATOR)
-    fun hentIdenterForAktor(@PathVariable aktorId: String): Aktor? {
+    fun hentIdenterForAktor(
+        @RequestParam aktorId: String,
+    ): Aktor? {
         clientIdValidation.validateClientId(
             ClientIdValidation.NamespaceAndApp(
                 namespace = "flex",
--- src/test/kotlin/no/nav/helse/flex/FlexIdenterCacheApiTest.kt
+++ src/test/kotlin/no/nav/helse/flex/FlexIdenterCacheApiTest.kt
@@ -3,29 +3,38 @@ package no.nav.helse.flex
 import no.nav.helse.flex.model.Aktor
 import no.nav.helse.flex.model.Identifikator
 import no.nav.helse.flex.model.Type
+import no.nav.helse.flex.util.OBJECT_MAPPER
 import no.nav.helse.flex.util.tilOsloZone
+import org.amshove.kluent.`should be equal to`
+import org.amshove.kluent.`should not be`
 import org.junit.jupiter.api.Test
 import org.springframework.http.MediaType
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import java.time.OffsetDateTime
 
-class FlexIdenterCacheApiTest: FellesTestOppsett() {
+class FlexIdenterCacheApiTest : FellesTestOppsett() {
     @Test
-    fun hentIdenterForAktorTest(){
-        val identifikator = Identifikator(
-            idnummer = "234567",
-            type = Type.FOLKEREGISTERIDENT,
-            gjeldende = true,
-            oppdatert = OffsetDateTime.now().tilOsloZone()
-        )
-        val aktor = Aktor("aktorId", listOf(identifikator))
+    fun hentIdenterForAktorTest()  {
+        val identifikator =
+            Identifikator(
+                idnummer = "234567",
+                type = Type.FOLKEREGISTERIDENT,
+                gjeldende = true,
+                oppdatert = OffsetDateTime.now().tilOsloZone(),
+            )
+        val aktor = Aktor("456745", listOf(identifikator))
         aktorService.lagreFlereAktorer(listOf(aktor))
 
-        mockMvc.perform(
-            get("/api/v1/identer/${aktor.aktorId}")
-                .header("Authorization", "Bearer ${skapAzureJwt("sykepengesoknad-backend-client-id")}")
-                .contentType(MediaType.APPLICATION_JSON)
-        ).andExpect(MockMvcResultMatchers.status().isOk).andReturn().response.contentAsString
+        val json =
+            mockMvc.perform(
+                post("/api/v1/identer/aktor")
+                    .header("Authorization", "Bearer ${skapAzureJwt("sykepengesoknad-backend-client-id")}")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .queryParam("aktorId", aktor.aktorId),
+            ).andExpect(MockMvcResultMatchers.status().isOk).andReturn().response.contentAsString
+
+        json `should not be` null
+        OBJECT_MAPPER.readValue(json, Aktor::class.java).aktorId `should be equal to` aktor.aktorId
     }
 }
